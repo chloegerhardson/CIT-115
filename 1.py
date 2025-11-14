3932f63a3e474358ca4fa07d6f5b5dd6ab5b6ac1
Chloe Gerhardson <cegerhardson@outlook.com>
2025-11-14 09:13:22 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+# Function Examples
+
+def greet():
+    print("Hello, world!")
+
+greet()
+
+def greet(name):
+    print("Hello,", name)
+
+greet("Chloe")
+
+def add(a, b):
+    return a + b
+
+result = add(3, 5)
+print(result)
